f9659c5b8569f0baa5d47db147dc995f78c02261
ahmed mohamed fathy <ahmedfathy99919@gmail.com>
2026-08-09 00:58:07 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ from rich.console import Console
 
 if __name__ == "__main__":
     console = Console()
-    console.print("[bold]Welcome to AI Researcher Agent[/bold]\n")
-    
-    model_choice = input("\n- Enter '1' for Gemini Model\n- Enter '2' for Groq/OpenAI Model\nChoose your perfered AI Model: ")
-    while model_choice != '1' and model_choice != '2':
-        model_choice = input("\n- Enter '1' for Gemini Model\n- Enter '2' for Groq/OpenAI Model\nChoose your perfered AI Model: ")
+    console.print("[bold]Welcome to AI Researcher Agent[/bold]")
+
+    model_choice = input(
+        "\n- Enter '1' for Gemini Model\n"
+        "- Enter '2' for Groq/OpenAI Model(in progress)\n"
+        "Choose your perfered AI Model: "
+    )
+    if model_choice == '2':
+        console.print(
+            f"\n[red][Groq/OpenAI Model is still in progress please choose another model.][/red]"
+        )
         
+    while model_choice != '1':
+        model_choice = input(
+            "\n- Enter '1' for Gemini Model\n"
+            "- Enter '2' for Groq/OpenAI Model(in progress)\n"
+            "Choose your perfered AI Model: "
+        )
+        if model_choice == '2':
+            console.print(
+                f"\n[red][Groq/OpenAI Model is still in progress please choose another model.][/red]"
+            )
+
     if model_choice == '1':
         start_gemini_agent()
-    else:
-        start_groq_agent()
+    elif model_choice == '2':
+        # start_groq_agent()
+        pass
