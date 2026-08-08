d8c3cf86f44ca3cf2a86b894c0f292dfc0346b83
ahmed mohamed fathy <ahmedfathy99919@gmail.com>
2026-08-09 00:40:00 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     while model_choice != '1' and model_choice != '2':
         model_choice = input("\n- Enter '1' for Gemini Model\n- Enter '2' for Groq/OpenAI Model\nChoose your perfered AI Model: ")
         
-    if model_choice == 1:
+    if model_choice == '1':
         start_gemini_agent()
     else:
         start_groq_agent()
